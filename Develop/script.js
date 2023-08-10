590b0294e59d5c2294422ba3d8593aed3b095932
Saiishago <mphagosk@gmail.com>
2023-08-09 17:59:52 -0700

    Updated index.html

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,17 @@
 const generateBtn = document.querySelector('#generate');
 var passwordLength = 10;
 var password = '';
+var event = Event
+var textAreaEl = document.querySelector('#textarea');
+var clearEl = document.querySelector('#clear');
 
 // Write password to the #password input
+function writePassword(event) {
+  var password = document.getElementById('password');
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
 function writePassword() {
   var password = document.getElementById('password');
   var passwordText = document.querySelector("#password");
@@ -16,7 +25,56 @@ const chars = {
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   numbers: "0123456789",
+  symbol: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 }
+
+const getChars = [
+  function upperCase() {
+    return chars.upperCase[Math.floor(Math.random() * chars.upperCase.length)];
+  },
+  function lowerCase() {
+    return chars.lowerCase[Math.floor(Math.random() * chars.lowerCase.length)];
+  },
+  function number() {
+    return chars.number[Math.floor(Math.random() * chars.number.length)];
+  },
+  function symbol() {
+    return chars.symbol[Math.floor(Math.random() * chars.symbol.length)];
+  }
+];
+
+const upper = document.getElementById("upperCase");
+const lower = document.getElementById("lowerCase");
+const number = document.getElementById("number");
+const symbol = document.getElementById("symbol");
+if (upper + lower + number + symbol === 0) {
+  alert('Check at least two boxes');
+
+}
+
+function generatePassword() {
+  const upper = document.getElementById("upperCase").checked;
+  const lower = document.getElementById("lowerCase").checked;
+  const number = document.getElementById("number").checked;
+  const symbol = document.getElementById("symbol").checked;
+  if (upper + lower + number + symbol === 0) {
+    alert('Check at least two boxes');
+  }
+  const passwordBox = document.getElementById('Your Secure Password');
+  const length = document.getElementById("length");
+  let password = "";
+  while (length.value > password.length) {
+    let keyToAdd = getChars[Math.floor(Math.random() * getChars.length)];
+    let isChecked = document.getElementById(keyToAdd.name).checked;
+    if (isChecked) {
+      password += keyToAdd();
+    }
+  }
+  passwordBox.innerHTML = password;
+}
+
+
+
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 16) + 46);
@@ -38,6 +96,36 @@ for (var i = 0; i <= passwordLength; i++) {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener('click', function (event) {
+  // Access value of pressed key with key property
+  var key = event.chars.toLowerCase();
+  var alphabetNumericCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split(
+    ''
+  );
+  if (alphabetNumericCharacters.includes(key)) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].textContent += event.key;
+    }
+  }
+});
+
+generateBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  textAreaEl.value = '';
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].textContent = '';
+  }
+});
+
+
+
+
+generateBtn.addEventListener('click', function() {
+  insertText = password
+  writePassword();
+})
 
 generateBtn.addEventListener("click", () => {
   const length = passwordLength.value;
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ These are the website links I looked at to get through this challenge:
 https://www.scaler.com/topics/password-generator-javascript/
 https://dev.to/code_mystery/random-password-generator-using-javascript-6a
 https://webdesign.tutsplus.com/build-a-configurable-random-password-generator-with-javascript--cms-93262t
+https://www.tutorialstonight.com/password-generator-in-javascript
